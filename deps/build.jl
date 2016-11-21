a1292a00e4f877d61e4d1c5ea1cc81dd8495c8ad
sdwfrost <sdwfrost@gmail.com>
2016-11-21 13:34:42 +0000

    Updated build.jl

--- deps/build.jl
+++ deps/build.jl
@@ -2,24 +2,33 @@ using BinDeps
 
 @BinDeps.setup
 
-cd("/Users/rveltz/work/prog_gd/julia/LSODA.jl/deps/")
-# https://github.com/sdwfrost/liblsoda
 liblsoda = library_dependency("liblsoda")
-BinDeps.provides(Sources,URI("https://github.com/sdwfrost/liblsoda/archive/master.tar.gz"),
-	liblsoda,unpacked_dir="liblsoda-master",installed_libpath="liblsoda-master/src/")
+BinDeps.provides(Sources,URI("https://github.com/sdwfrost/liblsoda/archive/master.tar.gz"),liblsoda,unpacked_dir="liblsoda-master")
 
-println(BinDeps.depsdir(liblsoda))
-srcdir = joinpath(BinDeps.depsdir(liblsoda),"src")
-srcdir = "/Users/rveltz/work/prog_gd/julia/LSODA.jl/deps/src/liblsoda-master"
+lsodadir = BinDeps.depsdir(liblsoda)
+
+libdir=joinpath(lsodadir,"usr","lib")
+
+srcdir = joinpath(lsodadir,"src","liblsoda-master")
+builddir = joinpath(srcdir,"src")
+
+if Sys.KERNEL == :Darwin
+	suffix="dylib"
+elseif Sys.KERNEL == :NT
+	suffix="dll"
+else
+	suffix="so"
+end
+
+
 BinDeps.provides(SimpleBuild,
     (@build_steps begin
         BinDeps.GetSources(liblsoda)
+				BinDeps.CreateDirectory(libdir)
         @build_steps begin
             BinDeps.ChangeDirectory(srcdir)
-            `make`
-			`pwd`
-			# `mkdir lib`
-			# `cp src/liblsoda.* lib`
+            BinDeps.MakeTargets()
+						`cp $builddir/liblsoda.$suffix $libdir`
         end
 end), liblsoda)
 @BinDeps.install Dict(:liblsoda => :liblsoda)
