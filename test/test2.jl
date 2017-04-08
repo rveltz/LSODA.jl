f7f5fabf983002a1cee66bf65c062e2e4009e895
Romain Veltz - Hebb <romain.veltz@inria.fr>
2017-04-08 14:32:42 +0200

    try to solve test issues

--- test/test2.jl
+++ test/test2.jl
@@ -8,7 +8,7 @@ function rhs!(t, x, ydot, data)
   nothing
 end
 
-rhs!(t, x, ydot) =  rhs!(t, x, ydot, nothing)
+rhs!(t, x, ydot) =  rhs!(t, x, ydot, 0.)
 
 y0 = [1.,0.,0.]
 println("\n####################################\n--> Use of a old wrapper for speed comparison")
@@ -19,17 +19,18 @@ tspan = [0., 0.4]
 # case with a vector
 println("\n####################################\n--> Use of a vector of times where output is required")
 tspan = [4.*10.0^k for k=-1:10]
-_, res = LSODA.lsoda(rhs!, y0, tspan, reltol= 1e-4,abstol = Vector([1.e-6,1.e-10,1.e-6]))
-_, res = @time LSODA.lsoda(rhs!, y0, tspan, reltol= 1e-4,abstol = Vector([1.e-6,1.e-10,1.e-6]))
+ctx, res = LSODA.lsoda(rhs!, y0, tspan, reltol= 1e-4,abstol = Vector([1.e-6,1.e-10,1.e-6]))
+ctx, res = @time LSODA.lsoda(rhs!, y0, tspan, reltol= 1e-4,abstol = Vector([1.e-6,1.e-10,1.e-6]))
 println(res)
+lsoda_free(ctx)
 
 # #case where we don't have to declare a new context
-# println("\n####################################\n--> Use of a lsoda_evolve!")
-# y0 = [1.,0.,0.]
-# tspan = [4.*10.0^k for k=-1:10]
-# ctx, _ = LSODA.lsoda(rhs!, y0, tspan[1:2], reltol= 1e-4,abstol = Vector([1.e-6,1.e-10,1.e-6]))
-# @time for k=2:length(tspan)
-# 	LSODA.lsoda_evolve!(ctx,y0,tspan[k-1:k])
-# 	@printf("at t = %12.4e y= %14.6e %14.6e %14.6e\n",tspan[k],y0[1], y0[2], y0[3])
-# end
+println("\n####################################\n--> Use of a lsoda_evolve!")
+y0 = [1.,0.,0.]
+tspan = [4.*10.0^k for k=-1:10]
+ctx, _ = LSODA.lsoda(rhs!, y0, tspan[1:2], reltol= 1e-4,abstol = Vector([1.e-6,1.e-10,1.e-6]))
+@time for k=2:length(tspan)
+	LSODA.lsoda_evolve!(ctx,y0,tspan[k-1:k])
+	@printf("at t = %12.4e y= %14.6e %14.6e %14.6e\n",tspan[k],y0[1], y0[2], y0[3])
+end
 lsoda_free(ctx)
